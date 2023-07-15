a42ee316ac04976c5dbfa58407e9fdef2cf63e54
anakonder <120507138+anakonder@users.noreply.github.com>
2023-07-15 17:35:41 +0300

    feedback-final

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -23,9 +23,9 @@ export default Statistics
 
 
 Statistics.propTypes = {
-    good: PropTypes.number,
-    neutral: PropTypes.number,
-    bad: PropTypes.number,
-    total: PropTypes.number,
-    positivePercentage: PropTypes.number
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    positivePercentage: PropTypes.number.isRequired
 }
